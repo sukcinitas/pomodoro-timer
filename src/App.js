8cb2af265ae72b42c085ee690a9c7acf1781ada8
Gintarė Černiauskaitė <gintare.cerniauskaite@protonmail.com>
2019-08-25 16:32:47 +0300

    React made prettier

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './App.css';
 
-let makeTime = function (sec) {
+//converts this.state.seconds to minutes:seconds format
+let makeTime = function(sec) {
   let timeInSec = sec;
   let minutes = Math.floor(timeInSec / 60);
   let seconds = timeInSec % 60;
@@ -9,7 +10,6 @@ let makeTime = function (sec) {
   seconds = String(seconds).padStart(2, "0");
   return minutes + ":" + seconds;
 }
-
 
 class App extends React.Component {
   constructor(props) {
@@ -19,81 +19,74 @@ class App extends React.Component {
       break: 5,
       seconds: 25 * 60,
       timerGoing: false,
-      styles: {},
+      styles: {}, //toggle session and break display
       timerLabel: "session",
-      goDisplay: {},
+      goDisplay: {display: "initial"}, //
       pauseDisplay: {display: "none"},
       animation: {animation: "blinking2 10000ms infinite"}
-
     }
+
     this.sessionInc = this.sessionInc.bind(this);
     this.sessionDec = this.sessionDec.bind(this);
     this.breakInc = this.breakInc.bind(this);
     this.breakDec = this.breakDec.bind(this);
     this.timerGo = this.timerGo.bind(this);
     this.timerPause = this.timerPause.bind(this);
-    this.timerGoGo = this.timerGoGo.bind(this);
+    this.timerTic = this.timerTic.bind(this);
     this.clear = this.clear.bind(this);
   }
   
   sessionInc() {
-    clearInterval(this.interval)
+    //clearInterval(this.interval);
+    //max session length is 60 minutes
     if (this.state.session === 60) {
-      this.setState ({
-      session: this.state.session
-    })
-    } else{
+      return;
+    } 
     this.setState ({
       session: this.state.session + 1,
       seconds: (this.state.session + 1) * 60,
     })
   }
-  }
   
   sessionDec() {
+    //min session length is 1 minute
     if (this.state.session === 1) {
-      this.setState ({
-      session: this.state.session
-    })
-    } else {
+      return;
+    } 
     this.setState ({
       session: this.state.session - 1,
       seconds: (this.state.session - 1) * 60,
     })
-    }
   }
   
   breakInc() {
-  if (this.state.break === 60) {
-    this.setState ({
-      break: this.state.break
-    })
-     } else {
+    //max break length is 60 minutes
+    if (this.state.break === 60) {
+      return;
+    } 
     this.setState ({
       break: this.state.break + 1
     })
   }
-     }
   
   breakDec() {
-  if (this.state.break === 1) {
-    this.setState ({
-      break: this.state.break
-    })
-     } else {
+    //min break length is 1 minute
+    if (this.state.break === 1) {
+      return;
+    } 
     this.setState ({
       break: this.state.break - 1
     })
-     }
   }
   
-  timerGoGo () {
+  timerTic () {
+    //depends on this.state.seconds
+    //if this.state.seconds during session becomes 0, play sound and set break seconds, if break seconds - 0 - vice versa, cycle repeats ad infinitum, unless cleared
     if(this.state.seconds === 0 && this.state.timerLabel === "session") {
       document.getElementById("beep").play();
       this.setState ({
-      seconds: this.state.break * 60,
-      timerLabel: "break"
-      
+        seconds: this.state.break * 60,
+        timerLabel: "break"
       })
     } else if (this.state.seconds === 0 && this.state.timerLabel === "break"){
       document.getElementById("beep").play();
@@ -101,8 +94,7 @@ class App extends React.Component {
       seconds: this.state.session * 60,
       timerLabel: "session"
       })
-    }
-    else {
+    } else {
     this.setState ({
       seconds: this.state.seconds - 1
     })
@@ -110,7 +102,8 @@ class App extends React.Component {
   }
 
   timerGo () {
-    this.interval = setInterval(this.timerGoGo, 1000)
+    //if timer starts going, session and break length selections are not displayed, go button icon is changed by pause icon
+    this.interval = setInterval(this.timerTic, 1000);
     this.setState ({
       timerGoing: !this.state.timerGoing,
       styles: {display: "none"},
@@ -121,6 +114,7 @@ class App extends React.Component {
   }
  
   timerPause () {
+    //pause button icon is changed by go button icon
     clearInterval(this.interval);
     this.setState ({
       timerGoing: !this.state.timerGoing,
@@ -133,9 +127,9 @@ class App extends React.Component {
   
   clear () {
     clearInterval(this.interval);
-      document.getElementById("beep").pause();
-      document.getElementById("beep").load();
-      this.setState ({
+    document.getElementById("beep").pause();
+    document.getElementById("beep").load();
+    this.setState ({
       styles: {},
       seconds: 25 * 60,
       session: 25, 
@@ -151,38 +145,41 @@ class App extends React.Component {
   render() {
     return (
       <div id="pomodoro" style={{}}>
+
         <div id="session" style={this.state.styles}>
           <label id="session-label">session length</label>
           <button class="decrement" onClick={this.sessionDec}><i className="fas fa-caret-down"></i></button>
           <span class="length">{this.state.session}</span>
           <button class="increment" onClick={this.sessionInc}><i className="fas fa-caret-up"></i></button>
         </div>
+
         <div id="break" style={this.state.styles}>
           <label id="break-label">break length</label>
           <button class="decrement" onClick={this.breakDec}><i className="fas fa-caret-down"></i></button>
           <span class="length">{this.state.break}</span>
           <button class="increment" onClick={this.breakInc}><i className="fas fa-caret-up"></i></button> 
         </div>
+
         <div id="timer">
+
           <h1 id="header">Pomodoro</h1>
+
           <div id="time-left">
             <p id="time-left-label">{makeTime(this.state.seconds)}</p>
-            
             <p id="timer-label" style={this.state.animation}>{this.state.timerLabel}</p>
             <button id="start_stop" onClick={this.state.timerGoing ? this.timerPause : this.timerGo}><i className="fas fa-play" style={this.state.goDisplay}></i><i className="fas fa-pause" style={this.state.pauseDisplay}></i></button>
             <button id="reset" onClick={this.clear}><i className="fas fa-undo-alt"></i></button>
             <audio id="beep"><source src="http://filmsound.org/starwars/wookie1.wav" />
             </audio>  
-         </div>
-         <h1 id="header2">Timer</h1>
-         
-          
+          </div>
+
+          <h1 id="header2">Timer</h1>
+
         </div>
+
        </div>
     )
   }
 } 
-
-
 
 export default App;
